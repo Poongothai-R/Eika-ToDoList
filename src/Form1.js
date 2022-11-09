3694e1ccdc409b348272fe98764cbf330441c56f
Poongothai-R <107067696+Poongothai-R@users.noreply.github.com>
2022-11-09 11:12:32 +0100

    completed form validation

--- src/Form1.js
+++ src/Form1.js
@@ -1,16 +1,32 @@
 import './Form1.css';
-import React, { useState, } from 'react';
+import React, {useEffect, useState,} from 'react';
 
 const GetItemInput = (props) => {
 
     const [itemName, setItemName] = useState('');
     const [itemPrice, setItemPrice] = useState('');
+    const [error,setError] = useState({});
+    const [isSubmit,setIsSubmit] = useState('false');
 
     const submitHandler = (e) => {
-        e.preventDefault();
+        e.preventDefault()
+       // if((e.target.itemName.value === "") || (e.target.itemPrice.value === ""))
+           setError(formValidate(e));
+        setIsSubmit('true');
         props.AddItem(e);
     }
-
+    useEffect(()=>{
+        if(Object.keys(error).length === 0 && isSubmit){
+            console.log(error);
+        }
+    },[error]);
+      const  formValidate = (e)=>{
+        if(e.target.itemName.value === "")
+            error.itemName = ("Item Name is Required");
+        if (e.target.itemPrice.value === "")
+            error.itemPrice = ("Item Price is Required");
+        return error;
+    };
 
     return (
         <div className="InputLst">
@@ -19,10 +35,12 @@ const GetItemInput = (props) => {
                 <label className="lbl_itm">Item Name: </label>
                 <input type="text" value={itemName} id="itemName" placeholder="Enter Item name" onChange={(e) => setItemName(e.target.value)}>
                 </input>
+                <label className="error"> <br/>{error.itemName}</label>
                 <br /> <br />
                 <label className="lbl_itm">Item Price: </label>
                 <input type="number" value={itemPrice} id="itemPrice" placeholder="Enter Number only" onChange={(e) => setItemPrice(e.target.value)}>
                 </input>
+                <label className="error"><br/>{error.itemPrice}</label>
                 <br /> <br /><div className="btn-save">
                 <button className="form_btn" type="submit" id="formButton">save</button></div>
             </form>
