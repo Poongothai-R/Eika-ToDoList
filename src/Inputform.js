5a87e1f878ac4f33566a6002a2492c8066974950
Poongothai-R <107067696+Poongothai-R@users.noreply.github.com>
2022-11-11 15:21:41 +0100

    Form1 Refactored.

--- src/Inputform.js
+++ src/Inputform.js
@@ -2,16 +2,18 @@ import './Inputform.css';
 import React, {useState,} from 'react';
 
 const GetItemInput = (props) => {
+
+    // Variable initialization for getting inputs, checking duplicate data and original data
+
+    const [itemName, setItemName] = useState('');
+    const [itemPrice, setItemPrice] = useState('');
+    const [error, setError] = useState({});
     let checkData = props.checkData;
     let duplicateData = checkData.map((name) => {
         return name.name.toUpperCase();
     });
 
-
-    const [itemName, setItemName] = useState('');
-    const [itemPrice, setItemPrice] = useState('');
-    const [error, setError] = useState({});
-
+    // Form Submission handler
 
     const submitHandler = (e) =>
     {
@@ -27,6 +29,7 @@ const GetItemInput = (props) => {
      let chkData = null;
      let dataList= (e.target.itemName.value.toString().toUpperCase());
 
+     // checking empty input fields and duplicate data
      if (e.target.itemName.value === "" || e.target.itemPrice.value === "")
      {
          if (e.target.itemName.value === "") itmName = "Item Name is Required";
@@ -49,24 +52,34 @@ const GetItemInput = (props) => {
 
     return (
         <div className="InputLst">
+
             <div className="InputLst_header"><h2>Add item</h2></div>
+
             <form className="InputLst_form" onSubmit={submitHandler}>
+
+                {/*label,input box and error message for item name */ }
                 <label className="lbl_itm">Item Name: </label>
                 <input type="text" value={itemName} id="itemName" placeholder="Enter Item name"
                        onChange={(e) => setItemName(e.target.value)}>
                 </input>
-                <label className="error"><br/> {error.itemName}</label>
+                <label className="error">{error.itemName}</label>
                 <br/>
+
+                {/*label,input box and error message for item price */ }
                 <label className="lbl_itm">Item Price: </label>
                 <input type="number" value={itemPrice} id="itemPrice" placeholder="Enter Number only"
                        onChange={(e) => setItemPrice(e.target.value)}>
                 </input>
-                <label className="error"><br/>{error.itemPrice}</label>
+                <label className="error">{error.itemPrice}</label>
                 <br/>
+
+                {/*button for add item */ }
                 <div className="btn-save">
                     <button className="form_btn" type="submit" id="formButton">save</button>
                 </div>
                 <br/>
+
+                {/*error message for duplicate data*/}
                 <label className="error">{error.checkData}</label>
             </form>
         </div>
